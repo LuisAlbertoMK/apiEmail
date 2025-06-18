58cf601a1f2f9773e5296c78c63f9416f2c99afd
LuisAlbertoMK <96153499+LuisAlbertoMK@users.noreply.github.com>
2025-06-18 15:59:00 -0600

    cambios revision

--- example.js
+++ example.js
@@ -6,7 +6,7 @@ const msg = {
   to: 'luis2016oro@gmail.com',
   from: 'ventas_culhuacan@speed-service.com.mx', // Use the email address or domain you verified above
   subject: 'XD',
-  text: 'aDDDnd easy to do anywhere, even with Node.js',
+  text: 'moviendo easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 
